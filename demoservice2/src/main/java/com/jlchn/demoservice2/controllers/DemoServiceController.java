be9bd647bf9afb770c6215b3148a4e13dd405806
jiangli <jianglichn@163.com>
2019-10-12 15:53:19 +0800

    sleuth and zipkin

--- demoservice2/src/main/java/com/jlchn/demoservice2/controllers/DemoServiceController.java
+++ demoservice2/src/main/java/com/jlchn/demoservice2/controllers/DemoServiceController.java
@@ -4,8 +4,11 @@ import java.util.Random;
 
 import com.jlchn.demoservice2.config.ServiceConfig;
 import com.jlchn.demoservice2.context.UserContextHolder;
+import com.netflix.discovery.converters.Auto;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,6 +23,9 @@ public class DemoServiceController {
     @Autowired
     private ServiceConfig serviceConfig;
 
+    @Autowired
+    private Tracer tracer;
+
     @RequestMapping(value="/",method = RequestMethod.GET)
     public String getLicenses() {
         randomSleep();
@@ -31,8 +37,10 @@ public class DemoServiceController {
     private void randomSleep(){
         Random rand = new Random();
         int num = rand.nextInt(3) + 1;
+        Span span = tracer.createSpan("readFromDatabase");
 
         if (num == 3) {
+
             try {
                 System.out.println("lets sleep 2 seconds");
                 Thread.sleep(2000);
@@ -40,5 +48,9 @@ public class DemoServiceController {
                 e.printStackTrace();
             }
         }
+
+        span.tag("peer.service", "database");
+        span.logEvent(Span.CLIENT_RECV);
+        tracer.close(span);
     }
 }
